0706a517cf88887e6c1e14f960cef1f277ee62bd
hellobro <alexczernik.plus@gmail.com>
2020-01-02 18:57:32 +0100

    Initial Commit

---
+++ GetColors2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetColors2
+//
+//  Created by Alexander Czernik on 02.01.20.
+//  Copyright © 2020 Alexander Czernik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
